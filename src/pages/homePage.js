e0d38d30550a73ce7e5dba0db9653aaa0720d688
Conor949 <20090029@mail.wit.ie>
2022-05-15 23:00:21 +0100

    Added filtering of movie list.

--- src/pages/homePage.js
+++ src/pages/homePage.js
@@ -32,4 +32,22 @@ const HomePage = (props) => {
     />
 );
 };
+
+const [nameFilter, setNameFilter] = useState("");
+const [genreFilter, setGenreFilter] = useState("0");
+
+const genreId = Number(genreFilter);
+
+let displayedMovies = movies
+  .filter((m) => {
+    return m.title.toLowerCase().search(nameFilter.toLowerCase()) !== -1;
+  })
+  .filter((m) => {
+    return genreId > 0 ? m.genre_ids.includes(genreId) : true;
+  });
+
+const handleChange = (type, value) => {
+  if (type === "name") setNameFilter(value);
+  else setGenreFilter(value);
+};
 export default HomePage;
